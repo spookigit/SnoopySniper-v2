69f06a3819ee6cb0edd7f8c3a6dadac2edb580ad
spookigit <contact@spookicards.com>
2025-08-07 21:56:24 -0500

    Create ConsolePage.xaml.cs

---
+++ SpookySniper/Pages/ConsolePage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SpookySniper.App.Pages
+{
+    public partial class ConsolePage : Page
+    {
+        public ConsolePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
