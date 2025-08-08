3867040cdb84f6015e637390d267c5c2abdec6f6
spookigit <contact@spookicards.com>
2025-08-07 20:34:09 -0500

    Update App.xaml.cs

--- SnoopySniper/App.xaml.cs
+++ SnoopySniper/App.xaml.cs
@@ -1,15 +1,26 @@
 using System.Windows;
-using SnoopySniper.Core;
+using SpookySniper.Core;
 using System.Threading.Tasks;
 using System;
 
-namespace SnoopySniper.App
+namespace SpookySniper.App
 {
     public partial class App : Application
     {
         protected override async void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            
+            // Check license first
+            if (!await LicenseManager.ValidateLicenseAsync())
+            {
+                var licenseWindow = new LicenseWindow();
+                if (licenseWindow.ShowDialog() != true)
+                {
+                    Shutdown();
+                    return;
+                }
+            }
             
             // Initialize the application
             await Startup.InitializeAsync();
