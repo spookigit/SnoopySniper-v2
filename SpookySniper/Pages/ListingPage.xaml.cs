cfdca4f82286d4078c2997ff858712fbdbdd3743
spookigit <contact@spookicards.com>
2025-08-07 21:59:02 -0500

    Create ListingPage.xaml.cs

---
+++ SpookySniper/Pages/ListingPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SpookySniper.App.Pages
+{
+    public partial class ListingPage : Page
+    {
+        public ListingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
