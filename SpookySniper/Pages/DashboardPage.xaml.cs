240a9c7dfcdf73a52dfdf1d0e6bb863126fe74e1
spookigit <contact@spookicards.com>
2025-08-07 21:49:11 -0500

    Create DashboardPage.xaml.cs

---
+++ SpookySniper/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SpookySniper.App.Pages
+{
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
