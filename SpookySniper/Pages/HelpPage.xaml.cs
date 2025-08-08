0395de67b7b92713d93140b40a217f448bad4ed3
spookigit <contact@spookicards.com>
2025-08-07 21:58:22 -0500

    Create HelpPage.xaml.cs

---
+++ SpookySniper/Pages/HelpPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace SpookySniper.App.Pages
+{
+    public partial class HelpPage : Page
+    {
+        public HelpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
